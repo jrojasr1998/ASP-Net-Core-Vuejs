937a3981def350da39dda9d8c77b02ea65b4f85c
jrojasr98 <jrojasr.20.04@gmail.com>
2023-02-02 18:08:15 +0000

    Test commit
    test

---
+++ Sistema/Sistema.Datos/Mapping/Almacen/CategoriaMap.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Sistema.Entidades.Almacen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sistema.Datos.Mapping.Almacen
+{
+    public class CategoriaMap : IEntityTypeConfiguration<Categoria>
+    {
+        public void Configure(EntityTypeBuilder<Categoria> builder)
+        {
+            builder.ToTable("categoria") // Se indca el nombre de la tabla
+                .HasKey(c => c.idcategoria); // se indica la llave primaria
+            builder.Property(c => c.nombre)
+                    .HasMaxLength(50);
+            builder.Property(c => c.descripcion)
+                    .HasMaxLength(256);
+        }
+    }
+}
---
+++ Sistema/Sistema.Entidades/Almacen/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Sistema.Entidades.Almacen
+{
+    public class Categoria
+    {
+        public int idcategoria { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3, ErrorMessage = "El nombre no debe de tener mas de 50 caracteres, ni m de 3 caracteres")]
+        public string nombre { get; set; }
+        [StringLength (256)]
+        public string descripcion { get; set; }
+        public bool condicion { get; set; }
+    }
+}
